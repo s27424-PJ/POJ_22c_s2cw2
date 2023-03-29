d08607d08d690e305e9f77ae40d3fec9575479a5
unknown <s27424@pjwstk.edu.pl>
2023-03-29 12:34:51 +0200

    do_zadania_wpisujemy_true_albo_false

--- z8/Main.java
+++ z8/Main.java
@@ -55,6 +55,7 @@ public class Main {
 
     }
     public static void main(String[] args) {
+	// wpisujemy true albo false
         boolean p[] = new boolean[4];
         boolean q[] = new boolean[4];
         Scanner scanner = new Scanner(System.in);
